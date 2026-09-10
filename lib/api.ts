ee8391d620de9a2244d151fd8011da3e9612554a
flyychmoreeee <mrasyazildan@gmail.com>
2026-09-10 21:15:47 +0700

    first commit

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import { ApiResponse, Course, Task } from "@/types";
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000/api";
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://nara.trivox.id/api";
 
 export class ApiError extends Error {
   status: number;
